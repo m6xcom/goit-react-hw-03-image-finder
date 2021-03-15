e66ae0c1146b6fa98a2e33a74348864f6849fd4c
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-03-15 15:44:35 +0200

    Adds code of app

--- src/services/pixabayApi.js
+++ src/services/pixabayApi.js
@@ -3,11 +3,14 @@ import axios from "axios";
 async function getFetch(query, page) {
   const key = "19787532-99c32fb0a864719ee4b9d7cb0";
   let url = `https://pixabay.com/api/?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`;
-  const photos = await axios
-    .get(url)
-    .then((response) => response.data)
-    .then((data) => data.hits);
-  return photos;
+  try {
+    const response = await axios.get(url);
+    const data = await response.data;
+    const photos = await data.hits;
+    return photos;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default getFetch;
